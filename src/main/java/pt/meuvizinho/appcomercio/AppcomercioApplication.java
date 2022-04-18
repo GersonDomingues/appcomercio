cd22c642a3a2e35bcf6558422f7a3a37fb42207f
Gerson Domingues <gersondsnetto@gmail.com>
2022-04-18 21:43:38 +0100

    Criado a operação de instanciação

--- src/main/java/pt/meuvizinho/appcomercio/AppcomercioApplication.java
+++ src/main/java/pt/meuvizinho/appcomercio/AppcomercioApplication.java
@@ -1,11 +1,20 @@
 package pt.meuvizinho.appcomercio;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import pt.meuvizinho.appcomercio.domain.Categoria;
+import pt.meuvizinho.appcomercio.repositories.CategoriaRepository;
+
 @SpringBootApplication
 public class AppcomercioApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppcomercioApplication.class, args);
@@ -14,6 +23,15 @@ public class AppcomercioApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(1, "Prato do dia");
+		Categoria cat2 = new Categoria(2, "Prato Entrada");
+		Categoria cat3 = new Categoria(3, "Prato Refeição");
+		Categoria cat4 = new Categoria(4, "Sobremesa");
+		Categoria cat5 = new Categoria(5, "Lanche");
+		Categoria cat6 = new Categoria(6, "Bebida");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6)); 
 		
 	}
 
