c4810e87151cf55dfa8a23e9667e7451773fffd8
Gerson Domingues <gersondsnetto@gmail.com>
2022-04-20 11:41:22 +0100

    Liberado CrossOrigin para acesso para teste no Ionic e refatorado o pom.xml

--- src/main/java/pt/meuvizinho/appcomercio/resources/CategoriaResource.java
+++ src/main/java/pt/meuvizinho/appcomercio/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import pt.meuvizinho.appcomercio.domain.Categoria;
 import pt.meuvizinho.appcomercio.services.CategoriaService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
