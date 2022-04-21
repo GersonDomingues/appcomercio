bb72dad7ca11818de0cad4a4eb3daaf53a5c6c09
Gerson Domingues <gersondsnetto@gmail.com>
2022-04-21 23:08:07 +0100

    Criado Categoria e Produto DTO, refatorado o projeto

--- src/main/java/pt/meuvizinho/appcomercio/services/CategoriaService.java
+++ src/main/java/pt/meuvizinho/appcomercio/services/CategoriaService.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import pt.meuvizinho.appcomercio.domain.Categoria;
+import pt.meuvizinho.appcomercio.dto.CategoriaDTO;
 import pt.meuvizinho.appcomercio.repositories.CategoriaRepository;
 
 @Service
@@ -20,8 +24,21 @@ public class CategoriaService {
 		return obj.orElse(null);
 	}
 
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
 
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
 }
--- src/main/java/pt/meuvizinho/appcomercio/services/ProdutoService.java
+++ src/main/java/pt/meuvizinho/appcomercio/services/ProdutoService.java
@@ -4,9 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import pt.meuvizinho.appcomercio.domain.Categoria;
 import pt.meuvizinho.appcomercio.domain.Produto;
+import pt.meuvizinho.appcomercio.repositories.CategoriaRepository;
 import pt.meuvizinho.appcomercio.repositories.ProdutoRepository;
 
 @Service
@@ -14,6 +19,9 @@ public class ProdutoService {
 
 	@Autowired
 	private ProdutoRepository repo;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public Produto find(Integer id) {
 		Optional<Produto> obj = repo.findById(id);
@@ -24,4 +32,10 @@ public class ProdutoService {
 		return repo.findAll();
 	}
 
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy,
+			String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
 }
